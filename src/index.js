700098eda34cf29779d813b828f6cd3ec1df8daf
paulsplanet <71366478+paulsplanet@users.noreply.github.com>
2021-09-20 16:48:40 -0700

    Vanilla-Reudx Counter

--- src/index.js
+++ src/index.js
@@ -4,24 +4,33 @@ const plus = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+number.innerText = 0;
 
-let count = 0;
+const ADD = "ADD";
+const MINUS = "MINUS"
 
-number.innerText = count;
+const countReducer = (count = 0, action) => {
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
+};
 
-const updateText = () => {
-  number.innerText = count;
+const countStore = createStore(countReducer);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
 }
 
-const handleAdd = () => {
-  count = count + 1;  
-  updateText();
-};
+countStore.subscribe(onChange);
 
-const handleMinus =() => {
-  count = count - 1;
-  updateText();
-};
+const handleMinus = () => {
+  countStore.dispatch({ type: MINUS });
+}
 
-plus.addEventListener("click", handleAdd);
+plus.addEventListener("click", () => countStore.dispatch({ type: ADD }));
 minus.addEventListener("click", handleMinus);
